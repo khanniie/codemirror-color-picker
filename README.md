c8763e07b6d2195fe0d475633fa662153ccaaa05
Connie Ye <constanceye1316@gmail.com>
2026-03-15 22:28:43 -0700

    Reorganize folder, add LICENSE, add example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# codemirror-color-picker
-Modified from @uiw's CM6 color picker extension, this color picker also supports Javascript. Meant for use in the p5.js web editor.
+# Codemirror Colorpicker Extension
+
+Modified from [@uiw's CM6 color picker extension](https://github.com/uiwjs/react-codemirror/tree/master), this color picker also supports Javascript.
+
+Meant for use in the p5.js web editor.
---
+++ example/example.js
@@ -0,0 +1,22 @@
+import { basicSetup, EditorView } from "codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { css } from "@codemirror/lang-css";
+import { color } from "../src";
+
+const jsContainer = document.querySelector(".javascript-container");
+const cssContainer = document.querySelector(".css-container");
+
+new EditorView({
+  doc: "console.log('#fff');\n",
+  extensions: [basicSetup, javascript(), color],
+  parent: jsContainer,
+});
+
+const CSS_DOCUMENT = `body {
+    color: rgba(0, 0, 0, 1);
+}`;
+new EditorView({
+  doc: CSS_DOCUMENT,
+  extensions: [basicSetup, css(), color],
+  parent: cssContainer,
+});
